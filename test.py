dbaae3a1c537c4ba7a775057d50f94223a9578f9
infGAI <infgai@yandex.ru>
2024-02-16 12:58:04 +0300

    added bootstrap template with cats

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import json
+import os
 
+from PIL import Image
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 
@@ -21,6 +23,16 @@ def index():
 @app.route('/odd_even/<num>')
 def odd_even(num):
     return render_template('index_jinja.html', number=int(num))
+
+@app.route('/carousel')
+def carousel():
+    path = os.path.join('static', 'carousel')
+    images=[os.path.join(path, img) for img in os.listdir(path)]
+    # чтобы убедиться, что картинки доступны можно их посмотреть
+    # for img in images:
+    #     opened_image = Image.open(img)
+    #     opened_image.show()
+    return render_template('carousel_bootstrap.html', img=images)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
